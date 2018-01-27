98c87596b2d089ccf24bc50d5264c851dcda7c2b
Apostolos Delis <apostolos.delis@yahoo.com>
2018-01-27 14:08:31 -0800

    Add files via upload

--- SafeT/AppDelegate.swift
+++ SafeT/AppDelegate.swift
@@ -2,8 +2,8 @@
 //  AppDelegate.swift
 //  SafeT
 //
-//  Created by Jason Ho on 12/21/17.
-//  Copyright © 2017 Jason Ho. All rights reserved.
+//  Created by Jason Ho on 1/27/18.
+//  Copyright © 2018 Jason Ho. All rights reserved.
 //
 
 import UIKit
--- SafeT/ViewController.swift
+++ SafeT/ViewController.swift
@@ -1,92 +1,130 @@
 //
 //  ViewController.swift
-//  SafeT
+//  MapBox Navigation Test
 //
-//  Created by Jason Ho on 12/21/17.
-//  Copyright © 2017 Jason Ho. All rights reserved.
+//  Created by Jason Ho on 1/26/18.
+//  Copyright © 2018 Jason Ho. All rights reserved.
 //
 
 import UIKit
 import Mapbox
-import Alamofire
-import SwiftyJSON
-//import MapboxNavigation
-//import MapboxDirections
-//import MapboxCoreNavigation
-
+import MapboxCoreNavigation
+import MapboxDirections
+import MapboxNavigation
+import MapboxMobileEvents
 
 class ViewController: UIViewController, MGLMapViewDelegate {
     
-//    var mapView: NavigationMapView!
-//    var directionsRoute: Route?
+    var mapView: NavigationMapView!
+    var directionsRoute: Route?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let styleURL = URL(string: "mapbox://styles/stitchy/cjbzqgu1q0lyq2rp7fnp25th0")
-        // Local paths are also acceptable.
         
-        let mapView = MGLMapView(frame: view.bounds, styleURL: styleURL)
+        mapView = NavigationMapView(frame: view.bounds, styleURL: styleURL)
+        
         mapView.setCenter(CLLocationCoordinate2D(latitude: 34.0522, longitude: -118.2437), zoomLevel: 10.5, animated: false)
-        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         
         view.addSubview(mapView)
         
+        // Set the map view's delegate
+        mapView.delegate = self
         
         
+        // Allow the map to display the user's location
+        mapView.showsUserLocation = true
+        mapView.setUserTrackingMode(.follow, animated: true)
         
-//        // Do any additional setup after loading the view, typically from a nib.
-//
-//        let mapView = MGLMapView(frame: view.bounds, styleURL: MGLStyle.lightStyleURL())
-//        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-//        mapView.tintColor = .darkGray
-//
-//        // Set the map’s center coordinate and zoom level.
-//        mapView.setCenter(CLLocationCoordinate2D(latitude: 34.0522, longitude: -118.2437), zoomLevel: 10.5, animated: false)
-//
-////        mapView = NavigationMapView(frame: view.bounds)
-//
-//        view.addSubview(mapView)
-//
-//        mapView.delegate = self
+        // Add a gesture recognizer to the map view
+        let setDestination = UILongPressGestureRecognizer(target: self, action: #selector(didLongPress(_:)))
+        mapView.addGestureRecognizer(setDestination)
         
         
     }
     
-//    func mapView(_ mapView: MGLMapView, didFinishLoading style: MGLStyle) {
-//
-//        let source = MGLVectorSource(identifier: "trees", configurationURL: URL(string: "mapbox://styles/stitchy/cjbzqgu1q0lyq2rp7fnp25th0")!)
-//
-//        style.addSource(source)
-//
-//        let layer = MGLCircleStyleLayer(identifier: "tree-style", source: source)
-//
-//        // The source name from the source's TileJSON metadata: mapbox.com/api-documentation/#retrieve-tilejson-metadata
-//        layer.sourceLayerIdentifier = "geodata_day_0_6-a2oj8m"
-//
-//        let stops = [
-//            0: MGLStyleValue(rawValue: UIColor(red:1.00, green:0.72, blue:0.85, alpha:1.0)),
-//            2: MGLStyleValue(rawValue: UIColor(red:0.69, green:0.48, blue:0.73, alpha:1.0)),
-//            4: MGLStyleValue(rawValue: UIColor(red:0.61, green:0.31, blue:0.47, alpha:1.0)),
-//            7: MGLStyleValue(rawValue: UIColor(red:0.43, green:0.20, blue:0.38, alpha:1.0)),
-//            16: MGLStyleValue(rawValue: UIColor(red:0.33, green:0.17, blue:0.25, alpha:1.0))
-//        ]
-//
-//        layer.circleColor = MGLStyleValue<UIColor>(interpolationMode: .interval,
-//                                                   sourceStops: stops,
-//                                                   attributeName: "AGE",
-//                                                   options: nil)
-//
-//        layer.circleRadius = MGLStyleValue(rawValue: 3)
-//
-//        style.addLayer(layer)
-//    }
-
+    @objc func didLongPress(_ sender: UILongPressGestureRecognizer) {
+        guard sender.state == .began else { return }
+        
+        // Converts point where user did a long press to map coordinates
+        let point = sender.location(in: mapView)
+        let coordinate = mapView.convert(point, toCoordinateFrom: mapView)
+        
+        // Create a basic point annotation and add it to the map
+        let annotation = MGLPointAnnotation()
+        annotation.coordinate = coordinate
+        annotation.title = "Start navigation"
+        mapView.addAnnotation(annotation)
+        
+        // Calculate the route from the user's location to the set destination
+        calculateRoute(from: (mapView.userLocation!.coordinate), to: annotation.coordinate) { (route, error) in
+            if error != nil {
+                print("Error calculating route")
+            }
+        }
+        
+    }
+    
+    // Calculate route to be used for navigation
+    func calculateRoute(from origin: CLLocationCoordinate2D,
+                        to destination: CLLocationCoordinate2D,
+                        completion: @escaping (Route?, Error?) -> ()) {
+        
+        // Coordinate accuracy is the maximum distance away from the waypoint that the route may still be considered viable, measured in meters. Negative values indicate that a indefinite number of meters away from the route and still be considered viable.
+        let origin = Waypoint(coordinate: origin, coordinateAccuracy: -1, name: "Start")
+        let destination = Waypoint(coordinate: destination, coordinateAccuracy: -1, name: "Finish")
+        
+        // Specify that the route is intended for automobiles avoiding traffic
+        let options = NavigationRouteOptions(waypoints: [origin, destination], profileIdentifier: .automobileAvoidingTraffic)
+        
+        // Generate the route object and draw it on the map
+        _ = Directions.shared.calculate(options) { [unowned self] (waypoints, routes, error) in
+            self.directionsRoute = routes?.first
+            // Draw the route on the map after creating it
+            self.drawRoute(route: self.directionsRoute!)
+        }
+    }
+    
+    func drawRoute(route: Route) {
+        guard route.coordinateCount > 0 else { return }
+        // Convert the route’s coordinates into a polyline
+        var routeCoordinates = route.coordinates!
+        let polyline = MGLPolylineFeature(coordinates: &routeCoordinates, count: route.coordinateCount)
+        
+        // If there's already a route line on the map, reset its shape to the new route
+        if let source = mapView.style?.source(withIdentifier: "route-source") as? MGLShapeSource {
+            source.shape = polyline
+        } else {
+            let source = MGLShapeSource(identifier: "route-source", features: [polyline], options: nil)
+            
+            // Customize the route line color and width
+            let lineStyle = MGLLineStyleLayer(identifier: "route-style", source: source)
+            lineStyle.lineColor = MGLStyleValue(rawValue:  _ColorLiteralType(red: 0.1897518039, green: 0.3010634184, blue: 0.7994888425, alpha: 1))
+            lineStyle.lineWidth = MGLStyleValue(rawValue: 3)
+            
+            // Add the source and style layer of the route line to the map
+            mapView.style?.addSource(source)
+            mapView.style?.addLayer(lineStyle)
+        }
+    }
+    
+    func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
+        return true
+    }
+    
+    // Present the navigation view controller when the callout is selected
+    func mapView(_ mapView: MGLMapView, tapOnCalloutFor annotation: MGLAnnotation) {
+        let navigationViewController = NavigationViewController(for: directionsRoute!)
+        self.present(navigationViewController, animated: true, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
+
 
